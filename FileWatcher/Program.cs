0881bcaaed83cf957636bb4799a8044149ae065f
Zeeshaan <zeeshaanbond@yahoo.com>
2018-04-09 10:12:26 +0530

    Completed Other functions. ToDo: Cutomised events

--- FileWatcher/Program.cs
+++ FileWatcher/Program.cs
@@ -6,22 +6,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Timers;
+//https://weblogs.asp.net/ashben/31773
 
 namespace FileWatcher
 {
     class Program
     {
+        static Dictionary<string, FileDetails> fileDetails = new Dictionary<string, FileDetails>();
+        static FileSystemWatcher watcher = new FileSystemWatcher();
         static int counter = 0;
         static void Main(string[] args)
         {
-           
-
             if (args.Length < 2)
             {
                 Console.WriteLine("Directory path or file format is missing");
                 Console.ReadLine();
-            }
-            
+            } 
             else
             {
                 string path = args[0].Trim();
@@ -29,11 +29,11 @@ namespace FileWatcher
                 Timer timer = new Timer(10000);
                 timer.Elapsed +=  (sender, e) =>  HandleTimer(path,filePattern);
                 timer.Start();
-                Console.Write("Press any key to exit... ");
-                Console.ReadKey();
-               
-                  
 
+                //Watch(path, filePattern);
+
+                Console.Write("Press any key to exit... \n");
+                Console.ReadKey();
             }
         }
 
@@ -65,44 +65,108 @@ namespace FileWatcher
 
         public static async void WatchDirectory(string targetDirectory,string filePattern)
         {           
-            await Task.Run(() => watch(targetDirectory, filePattern));
+            await Task.Run(() => Watch(targetDirectory, filePattern));
         }
 
      
-        private static void watch(string path,string filePattern )
+        private static void Watch(string path,string filePattern )
         {
-            FileSystemWatcher watcher = new FileSystemWatcher();
+            //FileSystemWatcher watcher = new FileSystemWatcher();
             watcher.Path = path;
             watcher.NotifyFilter = NotifyFilters.LastWrite;
             watcher.Filter = filePattern;
-            watcher.Created += new FileSystemEventHandler(OnCreated);
-            watcher.Changed += new FileSystemEventHandler(OnChanged);
-            watcher.Deleted += new FileSystemEventHandler(OnChanged);
+            watcher.Created += OnCreated;
+            watcher.Changed += OnChanged;
+            watcher.Deleted += OnDeleted;
             watcher.EnableRaisingEvents = true;
+            watcher.NotifyFilter = NotifyFilters.Attributes |
+                                    NotifyFilters.CreationTime |
+                                    NotifyFilters.FileName |
+                                    //NotifyFilters.LastAccess |
+                                    NotifyFilters.LastWrite |
+                                    //NotifyFilters.Size |
+                                    NotifyFilters.Security;
         }
 
         private static void OnCreated(object sender, FileSystemEventArgs e)
         {
-            Console.WriteLine("OnCreated Event Trigger");
+            FileDetails fd = new FileDetails() {
+                FileName = e.Name,
+                FilePath = e.FullPath,
+                TimeCreated = new FileInfo(e.FullPath).CreationTime,
+                TimeModified = new FileInfo(e.FullPath).LastWriteTime,
+            };
             StringBuilder sb = new StringBuilder();
-            sb.Append("Name: "+e.Name);           
-            sb.Append(" ");
-            sb.Append("Event Type: "+e.ChangeType.ToString());
-            sb.Append("    ");
-            sb.Append("TimeStamp: "+DateTime.Now.ToString());
+            sb.Append(e.ChangeType.ToString());
+            sb.Append(" : ");
+            sb.Append(e.Name);
+            sb.Append(" : ");
+
+            int count = 0;
+            using (StreamReader r = new StreamReader(e.FullPath))
+            {
+                string line;
+                while ((line = r.ReadLine()) != null)
+                {
+                    count++;
+                }
+            }
+            fd.PreviousNumOfLines = fd.CurrentNumOfLines = count;
+            sb.Append(count);
+            fileDetails.Add(e.Name, fd);
             Console.WriteLine(sb);
         }
 
         private static void OnChanged(object source, FileSystemEventArgs e)
-        {            
+        {
+            int count = 0;
+            bool isFileAccessible = false;
+            while (!isFileAccessible)
+            {
+                try
+                {
+                    using (StreamReader r = new StreamReader(e.FullPath))
+                    {
+                        string line;
+                        while ((line = r.ReadLine()) != null)
+                        {
+                            count++;
+                        }
+                    }
+                    isFileAccessible = true;
+                    fileDetails[e.Name].TimeModified = DateTime.Now;
+                }
+                catch
+                {
+
+                }
+            }
+
             StringBuilder sb = new StringBuilder();
-            sb.Append("Name: " + e.Name);          
-            sb.Append(" ");
-            sb.Append("Event Type: " + e.ChangeType.ToString());
-            sb.Append("    ");
-            sb.Append("TimeStamp: " + DateTime.Now.ToString());
+            sb.Append(e.ChangeType.ToString());
+            sb.Append(" : ");
+            sb.Append(e.Name);
+            sb.Append(" : ");
+
+            fileDetails[e.Name].PreviousNumOfLines = fileDetails[e.Name].CurrentNumOfLines;
+            fileDetails[e.Name].CurrentNumOfLines = count;
+            sb.Append(fileDetails[e.Name].CurrentNumOfLines - fileDetails[e.Name].PreviousNumOfLines);
             Console.WriteLine(sb);
         }
 
+        private static void OnDeleted(object source, FileSystemEventArgs e)
+        {
+            Console.WriteLine(e.ChangeType + " : " + e.Name);
+            fileDetails.Remove(e.Name);
+        }
+    }
+    class FileDetails
+    {
+        public string FileName { get; set; } = String.Empty;
+        public string FilePath { get; set; } = String.Empty;
+        public DateTime TimeCreated { get; set; }
+        public DateTime TimeModified { get; set; }
+        public int PreviousNumOfLines { get; set; } = 0;
+        public int CurrentNumOfLines { get; set; } = 0;
     }
 }
